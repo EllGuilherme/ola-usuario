66cd8ffcc0b50a23d0a6ad52a7f374c0914211a0
EllGuilherme <grds1000@gmail.com>
2022-03-12 20:24:22 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string nome;
+
+Console.WriteLine("digite seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.WriteLine($"olá, {nome}"!);
+Console.ResetColor();
